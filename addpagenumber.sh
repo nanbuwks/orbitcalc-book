fa044288242e8d68a39e2baccdd889413d1d956a
nanbuwks <nanbuwks+github@nanbu.com>
2023-12-30 21:38:25 +0900

    20230813 C102 version

--- addpagenumber.sh
+++ addpagenumber.sh
@@ -1,1 +1,7 @@
-addpage -o orbitcalc-addpagenumber.pdf orbitcalc.pdf -k 2 -s 2 -y 30
+# pdftk orbitcalc.pdf cat 2-end output booksort.pdf
+#addpage -o orbitcalc-addpagenumber.pdf booksort.pdf -k 2 -s 2 -y 30
+#rm booksort.pdf
+addpage -o orbitcalc-temp.pdf orbitcalc.pdf -k 2 -s 2 -y 30
+pdftk A=orbitcalc.pdf B=orbitcalc-temp.pdf cat B1-r2 Aend output orbitcalc-addpagenumber.pdf
+rm orbitcalc-temp.pdf
+
